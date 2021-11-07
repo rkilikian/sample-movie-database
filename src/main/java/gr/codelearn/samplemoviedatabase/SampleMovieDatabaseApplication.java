afdedf81eabe660b4be461dbd5db609112993c81
Renia Kilikian <rkilikian@gmail.com>
2021-11-07 13:11:59 +0200

    Initial commit

---
+++ src/main/java/gr/codelearn/samplemoviedatabase/SampleMovieDatabaseApplication.java
@@ -0,0 +1,13 @@
+package gr.codelearn.samplemoviedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMovieDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMovieDatabaseApplication.class, args);
+	}
+
+}
